ede18c76756f96c24743ce698d015c13848bb7ab
albertclementellovera <albertclementellovera@gmail.com>
2023-06-09 14:42:23 -0600

    proyecto045

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  valor1!: number;
+  valor2!: number;
+  operacion: string = "ninguna";
+  resultado!: number;
+}
