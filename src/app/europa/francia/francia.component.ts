7a2c6923fd824692c64f739c70ddae3bf858096c
albertclementellovera <albertclementellovera@gmail.com>
2023-06-09 22:42:49 -0600

    proyecto054

---
+++ src/app/europa/francia/francia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-francia',
+  templateUrl: './francia.component.html',
+  styleUrls: ['./francia.component.css']
+})
+export class FranciaComponent {
+
+}
---
+++ src/app/europa/italia/italia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-italia',
+  templateUrl: './italia.component.html',
+  styleUrls: ['./italia.component.css']
+})
+export class ItaliaComponent {
+
+}
