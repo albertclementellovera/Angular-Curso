cc2c061aa8b2590a7ff7a8c6ea086a5fd0841aa3
albertclementellovera <albertclementellovera@gmail.com>
2023-06-09 19:14:05 -0600

    proyecto048

---
+++ proyecto024/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  valorSlider1=0;
+  valorSlider2=0;
+  valorSlider3=0;
+  suma=0;
+
+  cambiar() {
+    this.suma = this.valorSlider1 + this.valorSlider2 + this.valorSlider3;
+  }
+}
