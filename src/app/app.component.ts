5e46090903c5873fbca5605d757a36fdb67318d4
albertclementellovera <albertclementellovera@gmail.com>
2023-06-10 19:18:56 -0600

    proyecto059

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  resultado!: string;
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    mail: new FormControl('', [Validators.required, Validators.email]),
+    mensaje: new FormControl('', [Validators.required, Validators.maxLength(500)])
+  });
+
+  submit() {
+    if (this.formularioContacto.valid)
+      this.resultado = "Todos los datos son válidos";
+    else
+      this.resultado = "Hay datos inválidos en el formulario";
+  }
+}
