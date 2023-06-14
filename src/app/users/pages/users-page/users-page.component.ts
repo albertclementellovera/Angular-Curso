98495f0cb5ac1517f06b9c3b82ce7c92d4b4512f
albertclementellovera <albertclementellovera@gmail.com>
2023-06-13 19:38:52 -0600

    proyecto062

---
+++ src/app/users/pages/users-page/users-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/user-service.service';
+import { User } from '../../interfaces/users';
+import { filter } from 'rxjs';
+
+@Component({
+  templateUrl: './users-page.component.html',
+  styleUrls: ['./users-page.component.css']
+})
+export class UsersPageComponent implements OnInit {
+
+  public users: User[] = [];
+  public currentPage: number = 1;
+
+
+  constructor( private usersService: UsersService ) {}
+
+  ngOnInit(): void {
+    this.loadPage( this.currentPage );
+  }
+
+  loadPage( page: number ) {
+
+    this.usersService.loadPage( page )
+      .pipe(
+        filter( users => users.length > 0 )
+      )
+      .subscribe( users => {
+        console.log(users)
+        this.currentPage = page;
+        this.users = users;
+      })
+  }
+}
